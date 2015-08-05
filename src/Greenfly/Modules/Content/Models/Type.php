babc2a0e3657fc22a7d03c56d76ebbded7b72060
Stephen Miracle <sjumwh@gmail.com>
2015-08-05 18:12:46 -0400

    1. added new callback methods for getting types, tags and taxonomies. 2. added taxonomy_type table to installer 3. updated content model classes

--- src/Greenfly/Modules/Content/Models/Type.php
+++ src/Greenfly/Modules/Content/Models/Type.php
@@ -1,15 +1,1 @@
-<?php
-
-namespace Greenfly\Modules\Content\Models;
-
-use Greenfly\Modules\Model;
-
-class Type extends Model
-{
-    protected $fillable = ['name', 'data']; 
-
-    public function content()
-    {
-        return $this->hasMany('Greenfly\Modules\Content\Models\Type\Content');
-    }
-}
+<?phpnamespace Greenfly\Modules\Content\Models;use Greenfly\Modules\Model;class Type extends Model{    protected $fillable = ['name', 'data'];    /**     * @return \Illuminate\Database\Eloquent\Relations\HasMany     */    public function content()    {        return $this->hasMany('Greenfly\Modules\Content\Models\Type\Content');    }    /**     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany     */    public function taxonomies()    {        return $this->belongsToMany('Greenfly\Modules\Content\Models\Taxonomy');    }}
